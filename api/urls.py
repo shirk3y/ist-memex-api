428cba07a37c42a87160a9582e197dacc82a749a
Marti Martinez <martinezah@gmail.com>
2014-10-30 18:02:50 +0000

    Adding urls and API view skeleton classes

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import LogList, LogItem, LogSearch
 
 urlpatterns = patterns('',
     url(r'^/?$', 'api.views.index'),
+    url(r'^logs/?$', LogList.as_view(), name='log-list'),
+    url(r'^logs/(?i)(?P<key>[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})/?$', 
+            LogItem.as_view(), name='log-item'),
+    url(r'^logs/where/(?i)(?P<index>[0-9a-zA-Z$.!*()_+-]{1,48})/is/(?P<value>[0-9a-zA-Z$.!*()_+-]{1,160})/?$', 
+            LogSearch.as_view(), name='log-search-exact'),
+    url(r'^logs/where/(?i)(?P<index>[0-9a-zA-Z$.!*()_+-]{1,48})/like/(?P<prefix>[0-9a-zA-Z$.!*()_+-]{1,160})/?$', 
+            LogSearch.as_view(), name='log-search-prefix'),
+    url(r'^logs/where/(?i)(?P<index>[0-9a-zA-Z$.!*()_+-]{1,48})/from/(?P<start>[0-9a-zA-Z$.!*()_+-]{1,160})/to/(?P<end>[0-9a-zA-Z$.!*()_+-]{1,160})/?$', 
+            LogSearch.as_view(), name='log-search-range'),
     url(r'^debug/?$', 'api.views.debug'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -16,3 +17,30 @@ def debug(request):
     response = {
     }
     return HttpResponse(json.dumps(response, indent=2), content_type="application/json")
+
+class LogList(APIView):
+    def get(self, request, format=None):
+        response = {'type':'GET'}
+        return Response(response)
+    def post(self, request, format=None):
+        response = {'type':'POST'}
+        return Response(response)
+        
+class LogItem(APIView):
+    def get(self, request, key, format=None):
+        response = {'type':'GET'}
+        return Response(response)
+    def put(self, request, key, format=None):
+        response = {'type':'PUT'}
+        return Response(response)
+    def patch(self, request, key, format=None):
+        response = {'type':'PATCH'}
+        return Response(response)
+    def delete(self, request, key, format=None):
+        response = {'type':'DELETE'}
+        return Response(response)
+
+class LogSearch(APIView):
+    def get(self, request, index, value=None, prefix=None, start=None, end=None, format=None):
+        response = {'type':'GET'}
+        return Response(response)
