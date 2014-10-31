c3d2cfebbe2ee329e7e5a13aa8e7f500059a2039
Marti Martinez <martinezah@gmail.com>
2014-10-31 00:33:00 +0000

    adding session authentication for my own sanity

--- api/settings.py
+++ api/settings.py
@@ -97,6 +97,7 @@ TEMPLATE_DIRS = os.environ.get('MEMEX_API_TEMPLATE_DIRS', '').split(',')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
