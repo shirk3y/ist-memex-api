c3b37caa54c86c01bb36099683ec5168f35c0215
Marti Martinez <martinezah@gmail.com>
2014-11-19 18:17:15 +0000

    version bump

--- api/settings.py
+++ api/settings.py
@@ -8,7 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-MEMEX_API_VERSION = "2.4"
+MEMEX_API_VERSION = "2.5"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
